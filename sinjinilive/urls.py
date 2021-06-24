d90990f367dc8208fe6be5fbb00be816dd311ade
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2021-06-24 15:14:49 +0530

    Update urls.py

--- sinjinilive/urls.py
+++ sinjinilive/urls.py
@@ -18,10 +18,11 @@ from django.urls import path, include
 
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('liverates.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
